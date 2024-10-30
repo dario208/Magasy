b600dce4b915e3099df9d3ea0ea3a0c0dd3e0caf
dario208 <dario.tsiahoua@esti.mg>
2024-10-30 16:54:57 +0300

    Remove unused database configuration and session initialization

--- models/models.py
+++
@@ -1,8 +0,0 @@
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
-from sqlalchemy.ext.declarative import declarative_base
-engine = create_engine('postgresql://dario208:123456@localhost/magasy', echo=True)
-Base = declarative_base()
-Base.metadata.bind = engine
-DBSession = sessionmaker(bind=engine)
-session = DBSession()
