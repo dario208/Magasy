407088d75f46f14bd6cee6e90e263327c476aee0
dario208 <dario.tsiahoua@esti.mg>
2024-11-05 16:03:46 +0300

    refactor(add new services db): :art:

--- core/database.py
+++ core/database.py
@@ -5,11 +5,11 @@ from typing import Annotated
 from fastapi import Depends
 engine = create_engine(DATABASE_URL)
 
-def init_db():
-    SQLModel.metadata.create_all(engine)
+# def init_db():
+#     SQLModel.metadata.create_all(engine)
 
-def get_session():
-    with Session(engine) as session:
-        yield session
-SessionDep = Annotated[Session, Depends(get_session)]
+# def get_session():
+#     with Session(engine) as session:
+#         yield session
+# SessionDep = Annotated[Session, Depends(get_session)]
 
